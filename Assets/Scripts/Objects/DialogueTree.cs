4db06f0e39d74937313067812752ee9cccf136e7
Yu Ying Li Wen <usbthekey@gmail.com>
2023-10-11 03:39:03 -0400

    Added Modifiers to DialogueTree

--- Assets/Scripts/Objects/DialogueTree.cs
+++ Assets/Scripts/Objects/DialogueTree.cs
@@ -1,45 +1,34 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 
 using UnityEngine;
 
 public class DialogueTree : MonoBehaviour
 {
     private const String path = "Dialogues";
-    public struct Node
-    {
-        public int id;
-        public String dialogue;
-        public List<Node> children;
-
-        public Node(int id, String dialogue)
-        {
-            this.id = id;
-            this.dialogue = dialogue;
-            this.children = new List<Node>();
-        }
-    }
-
     private Node root;
 
-    public Node GetTree() => root;
 
     private void Awake()
     {
-        print("ASDF");
         try 
         {
             var dialogueFile = Resources.Load(path) as TextAsset;
             var lines = dialogueFile.text.Split(';');
 
-            List <Node> nodes = new List<Node>();
+            List<Node> nodes = new(); // Cache for ease of access
 
             foreach ( var line in lines )
             {
-                print(line);
-                String[] splits = line.Split(','); // id = 0; parent = 1; dialogue = 2
-                Node n = new Node(Convert.ToInt32(splits[0]), splits[2]);
+                String[] splits = line.Split(','); // id = 0; parent = 1; modifier = 2; modifier value = 3; dialogue = 4
+
+                Node.Modifier modifier = new()
+                {
+                    category = Node.Convert(splits[2]),
+                    value = Convert.ToInt32(splits[3])
+                };
+
+                Node n = new (Convert.ToInt32(splits[0]), modifier, splits[4]);
 
                 nodes.Add(n);
 
@@ -58,6 +47,45 @@ public class DialogueTree : MonoBehaviour
         catch(Exception e)
         {
             print(e.ToString() + " aka path DNE.");
+        }
+    }
+
+    public Node GetTree() => root;
+    //////
+
+
+
+    //////
+    public struct Node
+    {
+        public int id;
+        public String dialogue;
+        public List<Node> children;
+        public Modifier modifier;
+
+        public Node(int id, Modifier modifier, String dialogue)
+        {
+            this.id = id;
+            this.dialogue = dialogue;
+            this.children = new();
+            this.modifier = modifier;
+        }
+
+        public struct Modifier
+        {
+            public enum Category { NONE, ATTACK, HIT_POINTS, SPEED, SPREAD }
+
+            public Category category;
+            public int value;
+        }
+
+        public static Modifier.Category Convert(string modifier)
+        {
+            if (modifier == "ATTACK") return Modifier.Category.ATTACK;
+            else if (modifier == "HP") return Modifier.Category.HIT_POINTS;
+            else if (modifier == "SPEED") return Modifier.Category.SPEED;
+            else if (modifier == "SPREAD") return Modifier.Category.SPREAD;
+            else return Modifier.Category.NONE;
         }
     }
 }
