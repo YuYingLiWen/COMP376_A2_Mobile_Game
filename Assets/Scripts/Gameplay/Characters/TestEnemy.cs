bc104b7c01cfbb81a812a68e930ce5ba6d812eac
Yu Ying Li Wen <usbthekey@gmail.com>
2023-10-12 00:26:15 -0400

    Renamed TestEnemy to Enemy

--- Assets/Scripts/Gameplay/Characters/TestEnemy.cs
+++ Assets/Scripts/Gameplay/Characters/TestEnemy.cs
@@ -2,7 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestEnemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
+    [SerializeField] private SOEnemies so;
 
+    private Health health;
+
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
 }
