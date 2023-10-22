b2e89408d9d4768dd3545fa1afad1308ff6cf056
Yu Ying Li Wen <usbthekey@gmail.com>
2023-10-22 02:17:33 -0400

    Added scale and terrain size modifications

--- Assets/Scripts/Gameplay/Terrain.cs
+++ Assets/Scripts/Gameplay/Terrain.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Terrain : MonoBehaviour
 {
     [SerializeField] private GameObject floorTile;
+    [SerializeField] private float tileScale = 1.0f;
 
+    [SerializeField] int sizeX = 20;
+    [SerializeField] int sizeY = 20;
     private List<GameObject> tiles;
 
     private void Awake()
@@ -19,21 +22,16 @@ public class Terrain : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void MakeTerrain()
     {
-        for(int i = 0; i < 20; i ++)
+        for(int i = 0; i < sizeY; i ++)
         {
-            for(int j = 0; j < 20; j++)
+            for(int j = 0; j < sizeX; j++)
             {
                 var tile = Instantiate(floorTile);
-                tile.transform.position += Vector3.right * i + Vector3.forward * j;
+                tile.transform.position += Vector3.right * (i * tileScale) + Vector3.forward * (j * tileScale);
                 tile.transform.parent = transform;
+                tile.transform.localScale = Vector3.one * tileScale;
             }
         }
     }
